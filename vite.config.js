184e0de2dd98af558ad87c29c678542ce22c1520
senzj <151595404+senzj@users.noreply.github.com>
2025-11-05 00:14:20 +0800

    vite chunk size limit

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,17 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/js/lucideicons.js'
+            ],
             refresh: true,
         }),
         tailwindcss(),
     ],
+
+    build: {
+        chunkSizeWarningLimit: 1600
+    }
 });
