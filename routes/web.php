4f44ee817a600adf27c4c60d56db25ad1094bb9d
senzj <151595404+senzj@users.noreply.github.com>
2025-11-04 22:03:53 +0800

    lesson 13

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,44 @@
 <?php
 
-use App\Http\Controllers\BleepController;
+use App\Http\Controllers\Auth\Login;
+use App\Http\Controllers\Auth\Logout;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\Register;
+use App\Http\Controllers\BleepController;
 
+// REGISTER
+Route::view('/register', 'auth.register')
+    ->middleware('guest')
+    ->name('register');
+
+Route::post('/register', Register::class)
+    ->middleware('guest');
+
+
+// LOGIN
+Route::view('/login', 'auth.login')
+    ->middleware('guest')
+    ->name('login');
+
+Route::post('/login', Login::class)
+    ->middleware('guest');
+
+
+// LOGOUT
+Route::post('/logout', Logout::class)
+    ->middleware('auth')
+    ->name('logout');
+
+
+// Bleep Routes
 Route::get('/', [BleepController::class, 'index']);
 
-Route::post('/bleeps', [BleepController::class, 'store']);
-Route::get('/bleeps/{bleep}/edit', [BleepController::class, 'edit']);
-Route::put('/bleeps/{bleep}', [BleepController::class, 'update']);
-Route::delete('/bleeps/{bleep}', [BleepController::class, 'destroy']);
+Route::middleware('auth')->group((function () {
+
+    Route::post('/bleeps', [BleepController::class, 'store']);
+    Route::get('/bleeps/{bleep}/edit', [BleepController::class, 'edit']);
+    Route::put('/bleeps/{bleep}', [BleepController::class, 'update']);
+    Route::delete('/bleeps/{bleep}', [BleepController::class, 'destroy']);
+
+}));
+
