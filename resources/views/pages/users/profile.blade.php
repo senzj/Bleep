1930e2ab3c1d60b7436f14fea6122c258883ab6c
senzj <151595404+senzj@users.noreply.github.com>
2026-03-01 19:50:46 +0800

    fix: UI clean up - removed extra repost text

--- resources/views/pages/users/profile.blade.php
+++ resources/views/pages/users/profile.blade.php
@@ -230,18 +230,6 @@
                                     data-next-url="{{ $reposts->hasMorePages() ? route('user.reposts', ['username' => $user->username, 'page' => $reposts->currentPage() + 1]) : '' }}">
                                     @foreach($reposts as $repost)
                                         @if($repost->bleep && !$repost->bleep->deleted_at)
-                                            <div class="relative">
-                                                <div class="flex items-center gap-2 mb-2 text-xs text-base-content/60 pl-2">
-                                                    <i data-lucide="repeat" class="w-4 h-4"></i>
-                                                    <span>
-                                                        @if($isOwnProfile)
-                                                            You reposted
-                                                        @else
-                                                            {{ "@" . $user->username }} reposted
-                                                        @endif
-                                                        <span class="text-base-content/40">• {{ $repost->created_at->diffForHumans() }}</span>
-                                                    </span>
-                                                </div>
                                                 <x-bleep :bleep="$repost->bleep" />
                                             </div>
                                         @endif
